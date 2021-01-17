02a2fc667f4249572630c7473696278deedd269a
Richard <richardstoeffel47@gmail.com>
2021-01-17 17:42:23 -0500

    -NF: Add Initial models

--- uploader/models.py
+++ uploader/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    street_address = models.CharField(max_length=128)
+    state = models.CharField(max_length=128)
+    zip_code = models.CharField(max_length=128)
+
+class Transaction(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    purchase_status = models.CharField(max_length=16)
+    product_id = models.IntegerField()
+    product_name = models.CharField(max_length=1024)
+    purchase_amount = models.FloatField()
+    transaction_date = models.DateTimeField()
